79b529c688a426581499cb8cac39869977356b1b
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2024-11-12 16:09:58 +0700

    refactor: change println to printf (formatted output)

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ func main() {
 	const confTickets = 50;
 	var remTickets = 50;
 	
-	fmt.Println("Welcome to", confName, "booking app");
-	fmt.Println("We have total of", confTickets, "ticketes and", remTickets, "are still available.");
+	fmt.Printf("Welcome to %v booking app\n", confName);
+	fmt.Printf("We have total of %v tickets and %v are still available.\n", confTickets, remTickets);
 	fmt.Println("Get your tickets here to attend");
 }
