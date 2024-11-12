3b68014f9266a8799920bbc33f1be91056437025
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2024-11-12 16:06:08 +0700

    feat: add vars and consts

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!");
+	var confName = "Go Conference";
+	const confTickets = 50;
+	var remTickets = 50;
+	
+	fmt.Println("Welcome to", confName, "booking app");
+	fmt.Println("We have total of", confTickets, "ticketes and", remTickets, "are still available.");
+	fmt.Println("Get your tickets here to attend");
 }
