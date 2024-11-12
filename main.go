797f39fdc0bb98ec4d56a0c0b32a3db8fbf06faf
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2024-11-12 16:24:53 +0700

    feat: add pointers and more inputs

--- main.go
+++ main.go
@@ -13,13 +13,23 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", confTickets, remTickets);
 	fmt.Println("Get your tickets here to attend");
 
-	var username string;
+	var firstName string;
+	var lastName string;
+	var email string;
 	var userTickets uint;
 
 	// ask user for their name
-	fmt.Print("Enter username: ");
-	fmt.Scan(&username);
+	fmt.Print("Enter your first name: ");
+	fmt.Scan(&firstName);
 
-	userTickets = 2;
-	fmt.Printf("User %v booked %v tickets.\n", username, userTickets);
+	fmt.Print("Enter your last name: ");
+	fmt.Scan(&lastName);
+
+	fmt.Print("Enter your email address: ");
+	fmt.Scan(&email);
+
+	fmt.Print("Enter number of tickets: ");
+	fmt.Scan(&userTickets);
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v", firstName, lastName, userTickets, email);
 }
