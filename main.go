1a56fb7ad9e233152667e98b54f0c48e7ffdb688
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2024-11-12 16:15:09 +0700

    feat: add datatypes

--- main.go
+++ main.go
@@ -3,11 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	var confName = "Go Conference";
-	const confTickets = 50;
-	var remTickets = 50;
+	confName := "Go Conference";
+	const confTickets uint = 50;
+	var remTickets uint = 50;
+
+	fmt.Printf("confTickets is %T, remTickets is %T", confTickets, remTickets);
 	
 	fmt.Printf("Welcome to %v booking app\n", confName);
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", confTickets, remTickets);
 	fmt.Println("Get your tickets here to attend");
+
+	var username string;
+	var userTickets uint;
+
+	// ask user for their name
+
+	username = "Ben";
+	userTickets = 2;
+	fmt.Printf("User %v booked %v tickets.\n", username, userTickets);
 }
