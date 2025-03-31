d106b83b2e8fb1f9f22006cb7df3d76af8a00ed6
yehezkiel1086 <tsukikagemaikaka1@gmail.com>
2025-03-31 23:34:13 +0700

    feat(funcs): add basic-output

---
+++ funcs/basic-out/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Here's your name: ") // inline output
+	fmt.Println("Kenneth") // newline output
+	fmt.Printf("Your age: %d\nJob: %s\n", 28, "Game Dev") // formatted output
+	
+}
