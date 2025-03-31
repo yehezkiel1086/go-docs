182eacb06ba9a3b649c4202fdc3857eadec0801b
yehezkiel1086 <tsukikagemaikaka1@gmail.com>
2025-03-31 23:13:46 +0700

    feat(funcs): add hello

---
+++ calculator-cli/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"calculator/calculator"
+	"fmt"
+)
+
+func main() {
+	var res int = calculator.Add(2, 3)
+	fmt.Println(res)
+}
---
+++ input.go
@@ -0,0 +1,3 @@
+package funcs
+
+func inputFuncs() {}
