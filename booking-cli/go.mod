105f95b0a5866b036a3ff81e612cb9d6a338da1b
yehezkiel1086 <tsukikagemaikaka1@gmail.com>
2025-06-11 23:09:20 +0700

    feat(booking-cli): redo program

--- booking-cli/go.mod
+++ booking-cli/go.mod
@@ -1,3 +1,3 @@
-module booking-cli
+module booking-app
 
 go 1.23.3
--- booking-cli/main.go
+++ booking-cli/main.go
@@ -3,5 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	confName := "Go Conference" // syntactic sugar
+	const confTickets uint8 = 50
+	var remTickets uint8 = 50
+
+	fmt.Printf("VAR_TYPES: confTickets is %T, remTickets is %T, confName is %T\n", confTickets, remTickets, confName)
+	
+	fmt.Println("Welcome to", confName, "🥳")
+	fmt.Println("Note: Get your tickets here to attend.")
+	fmt.Printf("There are %v left out of %v\n", remTickets, confTickets)
+
+	var userName string
+	var userTickets uint8
+	var userEmail string
+
+	fmt.Printf("\nEnter your first name: ")
+	fmt.Scan(&userName)
+	fmt.Printf("Enter your email: ")
+	fmt.Scan(&userEmail)
+	fmt.Printf("Enter total tickets to order: ")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("\nThanks, %v for ordering %v tickets!\n", userName, userTickets)
+	fmt.Printf("You'll receive ticket at your email: %v\n\n", userEmail)
+	remTickets -= userTickets
+	fmt.Printf("Remaining tickets: %v\n", remTickets)
 }
