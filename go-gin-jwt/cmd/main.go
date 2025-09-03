4c06d42268684e9d88ae4d2a83dda45028613163
yehezkiel1086 <tsukikagemaikaka1@gmail.com>
2025-09-04 00:59:50 +0700

    feat(go-gin-jwt): init project and add registration

---
+++ go-gin-jwt/cmd/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/yehezkiel1086/go-docs/go-gin-jwt/internal/adapter/config"
+	"github.com/yehezkiel1086/go-docs/go-gin-jwt/internal/adapter/handler"
+	"github.com/yehezkiel1086/go-docs/go-gin-jwt/internal/adapter/storage/postgres"
+	"github.com/yehezkiel1086/go-docs/go-gin-jwt/internal/adapter/storage/postgres/repository"
+	"github.com/yehezkiel1086/go-docs/go-gin-jwt/internal/core/service"
+)
+
+func main() {
+	// get .env configs
+	config, err := config.Init()
+	if err != nil {
+		panic(err)
+	}
+
+	// init db
+	ctx := context.Background()
+	db, err := postgres.Init(ctx, config.DB)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Connected to Postgres!")
+	
+	// db migrations
+	if err := db.Migrate(); err != nil {
+		panic(err)
+	}
+	fmt.Println("Migration successful.")
+
+	// dependency injections
+	userRepo := repository.InitUserRepository(db)
+	userService := service.InitUserService(userRepo)
+	userHandler := handler.InitUserHandler(userService)
+
+	// routing
+	r, err := handler.InitRouter(
+		config.HTTP,
+		*userHandler,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Routes created successfully.")
+
+	// run the server
+	uri := fmt.Sprintf("%v:%v", config.HTTP.Host, config.HTTP.Port)
+
+	fmt.Println("Server is running on", uri)
+	if err := r.Serve(uri); err != nil {
+		panic(err)
+	}
+}
---
+++ go-gin-jwt/internal/adapter/storage/postgres/db.go
@@ -0,0 +1,37 @@
+package postgres
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/yehezkiel1086/go-docs/go-gin-jwt/internal/adapter/config"
+	"github.com/yehezkiel1086/go-docs/go-gin-jwt/internal/core/domain"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type DB struct {
+	db *gorm.DB
+}
+
+func Init(ctx context.Context, config *config.DB) (*DB, error) {
+	dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v sslmode=disable TimeZone=Asia/Shanghai", config.Host, config.User, config.Password, config.Name, config.Port)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return &DB{}, err
+	}
+
+	return &DB{db: db}, nil
+}
+
+func (db *DB) Migrate() error {
+	if err := db.db.AutoMigrate(&domain.User{}); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (db *DB) GetDB() *gorm.DB {
+	return db.db
+}
