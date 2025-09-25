5d063204b47792d20efaf2aeb9e11bf1690ddad5
yehezkiel1086 <tsukikagemaikaka1@gmail.com>
2025-09-25 12:31:00 +0700

    feat(go-concurrency): update goroutines

--- go-concurrency/goroutines/main.go
+++ go-concurrency/goroutines/main.go
@@ -5,73 +5,63 @@ import (
 	"time"
 )
 
-func simpleRoutine() {
-	fmt.Println("Simple Routine:")
+var start time.Time
 
+func concurrent() {
+	apiCall := func(name string) {
+		time.Sleep(time.Duration(100) * time.Millisecond)
+		fmt.Println("Api Call", name, "running at", time.Since(start))
+	}
+
+	go apiCall("A")
+	go apiCall("B")
+
+	time.Sleep(time.Duration(200) * time.Millisecond)
+	fmt.Println("Function ends at", time.Since(start))
+}
+
+func sequential() {
+	apiCall := func(name string) {
+		time.Sleep(time.Duration(100) * time.Millisecond)
+		fmt.Println("Api Call", name, "running at", time.Since(start))
+	}
+
+	apiCall("A")
+	apiCall("B")
+
+	time.Sleep(time.Duration(200) * time.Millisecond)
+	fmt.Println("Function ends at", time.Since(start))
+}
+
+func indecisive() {
+	sayHello := func(name string) {
+		fmt.Println("Hello", name)
+	}
+
+	go sayHello("Benjamin")
+	go sayHello("Elisabeth")
+
+	time.Sleep(time.Duration(10) * time.Millisecond)
+}
+
+func firstGoroutine() {
 	go func() {
-		fmt.Println("Simple Routine")
+		fmt.Println("Goroutine thread")
 	}()
 
-	fmt.Println("Routine start")
-	time.Sleep(time.Millisecond * 10)
-	fmt.Println("Routine end")
+	time.Sleep(time.Duration(10) * time.Millisecond)
 }
-
-func indecisiveRoutine() {
-	fmt.Println("Indecisive Routine:")
-
-	go func() {
-		fmt.Println("Routine A")
-	}()
-	go func() {
-		fmt.Println("Routine B")
-	}()
-
-	fmt.Println("Routine started")
-	time.Sleep(time.Millisecond * 10)
-	fmt.Println("Routine end")
-}
-
-func ApiCallA(start time.Time) {
-	time.Sleep(time.Millisecond * 100)
-	fmt.Println("API call A at:", time.Since(start))
-}
-
-func ApiCallB(start time.Time) {
-	time.Sleep(time.Millisecond * 100)
-	fmt.Println("API call B at:", time.Since(start))
-}
-
-func sequentialApiCall() {
-	fmt.Println("Sequential API call Begins:")
-	start := time.Now()
-	ApiCallA(start)
-	ApiCallB(start)
-
-	time.Sleep(time.Millisecond * 200)
-	fmt.Println("Sequential API call ends at:", time.Since(start))
-}
-
-func concurrentApiCall() {
-	fmt.Println("Concurrent API call Begins:")
-	start := time.Now()
-	go ApiCallA(start)
-	go ApiCallB(start)
-
-	time.Sleep(time.Millisecond * 200)
-	fmt.Println("Sequential API call ends at:", time.Since(start))
-}
-
-// from APICallA at time 111.1639ms
-// from APICallB at time 218.4708ms
-// from main function at time 420.4578ms
 
 func main() {
-	simpleRoutine()
-	fmt.Println()
-	indecisiveRoutine()
-	fmt.Println()
-	sequentialApiCall()
-	fmt.Println()
-	concurrentApiCall()
+	fmt.Println("Main thread start")
+
+	// firstGoroutine()
+	// indecisive()
+
+	// sequential vs concurrent
+	start = time.Now()
+	// concurrent()
+	sequential()
+	
+	fmt.Println("Main thread end")
 }
