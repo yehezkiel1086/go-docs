dc89d2f9d2789d85360a5f50b26da6d2a7ead9e4
yehezkiel1086 <tsukikagemaikaka1@gmail.com>
2025-10-09 16:37:44 +0700

    feat: add go-sinle-file-upload

--- go-oauth/internal/core/domain/auth.go
+++
@@ -1,6 +0,0 @@
-package domain
-
-type LoginReq struct {
-	Email    string `json:"email" binding:"required"`
-	Password string `json:"password" binding:"required"`
-}
---
+++ go-single-file-upload/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	// Set a lower memory limit for multipart forms (default is 32 MiB)
+	// router.MaxMultipartMemory = 8 << 20 // 8 MiB
+	router.POST("/upload", func(c *gin.Context) {
+		// single file
+		file, _ := c.FormFile("file")
+		log.Println(file.Filename)
+
+		// Upload the file to specific dst.
+		c.SaveUploadedFile(file, "./files/"+file.Filename)
+
+		c.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", file.Filename))
+	})
+	router.Run(":8080")
+}
