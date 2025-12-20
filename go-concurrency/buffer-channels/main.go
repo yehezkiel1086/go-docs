54fc7dfba4d8faba7d9e421caefd87ba1965c424
yehezkiel1086 <tsukikagemaikaka1@gmail.com>
2025-12-20 19:35:18 +0700

    feat(go-concurrency): update completed buffer-channels

--- go-concurrency/buffer-channels/main.go
+++ go-concurrency/buffer-channels/main.go
@@ -1,22 +1,93 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func sender(c chan int) {
-	for i := 0; i < 4; i++ {
-		fmt.Println("Goroutine send:", i)
-		c <- i // sends 4 times
+func send(c chan int) {
+	for i := range(3) {
+		fmt.Printf("send: %d\n", i)
+		c <- i
 	}
 }
 
-func receiver() {
-	c := make(chan int, 1) // size of buffer channel just 1
-	go sender(c)
-	for i := 0; i < 3; i++ {
-		fmt.Println("receiver receive:", <- c) // receives three times
+func capacityBlocking() {
+	c := make(chan int, 1)
+
+	go send(c)
+
+	for range(3) {
+		fmt.Printf("Receive: %d\n", <-c)
+	}
+}
+
+func infLoop() {
+	c := make(chan string, 1)
+
+	go func() {
+		for range(10) {
+			c <- "every 200 ms"
+			time.Sleep(200 * time.Millisecond)
+		}
+		close(c) // prevents deadlock
+	}()
+
+	for msg := range c {
+		fmt.Println(msg)
+	}
+}
+
+func infLoopClose() {
+	c := make(chan string, 5)
+
+	for i := range(5) {
+		c <- fmt.Sprintf("data %d", i)
+	}
+
+	close(c) // prevents deadlock
+
+	for msg := range c {
+		fmt.Println(msg)
+	}
+}
+
+var start time.Time
+
+func init() {
+	start = time.Now()
+}
+
+func multiChannels() {
+	c1 := make(chan string)
+	c2 := make(chan string)
+	go func() {
+		for {
+			time.Sleep(200 * time.Millisecond)
+			c1 <- "every 200 milliseconds"
+		}
+	}()
+
+	go func() {
+		for {
+			time.Sleep(500 * time.Millisecond)
+			c2 <- "every 500 milliseconds"
+		}
+	}()
+
+	for {
+		select {
+		case msg := <-c1:
+			fmt.Printf("%s channel 1. At time %s\n", msg, time.Since(start))
+		case msg := <-c2:
+			fmt.Printf("%s channel 2. At time %s\n", msg, time.Since(start))
+		}
 	}
 }
 
 func main() {
-		receiver()
+	// capacityBlocking()
+	// infLoop()
+	// infLoopClose()
+	multiChannels()
 }
